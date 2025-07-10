540a0e0fe29e003d1978d69937a8faddffda319f
abhinavjain3161 <Abhinav.Jain303@nmims.in>
2025-07-10 12:28:29 +0530

    Second Java Project

---
+++ Appointment.java
@@ -0,0 +1,19 @@
+package com.abhijain3161.blc;
+
+public class Appointment {
+	private Patient patient;
+    private Doctor doctor;
+    private String date;
+
+    public Appointment(Patient patient, Doctor doctor, String date) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment: [Patient: " + patient + ", Doctor: " + doctor + ", Date: " + date + "]";
+    }
+}
+
